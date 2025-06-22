9d1aad3551942297640045a76bd92a94a6e46192
ramakrishna67 <ramakrishnapaila64@gmail.com>
2025-06-22 18:45:54 +0530

    update host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-})
+  server: {
+    port: 5173,
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "https://zesty-backend-sd0l.onrender.com", // your backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
